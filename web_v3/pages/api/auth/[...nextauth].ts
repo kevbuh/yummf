6ff2feb2ac895fb5708c49e9ed434f39e0f9e603
Kevin Buhler <kevinbuhlerr@gmail.com>
2022-07-18 21:53:23 -0600

    added next auth to web_v3

---
+++ web_v3/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+// import FacebookProvider from "next-auth/providers/facebook";
+// import EmailProvider from "next-auth/providers/email"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.JWT_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
