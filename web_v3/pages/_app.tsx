fdf2a84ff89a9150656bcd7aaa3cfa0b5a39b430
Kevin Buhler <kevinbuhlerr@gmail.com>
2022-08-05 17:42:34 -0600

    added google adsense init code

--- web_v3/pages/_app.tsx
+++ web_v3/pages/_app.tsx
@@ -7,6 +7,8 @@ import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import Script from "next/script";
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = useState(
     () =>
@@ -27,6 +29,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
           name="description"
           content="Find your next favorite meal"
           key="desc"
+        />
+        <Script
+          id="Adsense-id"
+          data-ad-client="ca-pub-7158717683717233"
+          async
+          strategy="afterInteractive"
+          onError={(e) => {
+            console.error("Google AdSense Script failed to load", e);
+          }}
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         />
       </Head>
       <QueryClientProvider client={queryClient}>
