85a1bf4535aa41a41dd4079859f1f07dd85aa81f
Kevin Buhler <kevinbuhlerr@gmail.com>
2022-08-10 13:57:18 -0600

    fixed bugs to see test recipes page on prod

--- web_v3/pages/recipes/test.tsx
+++ web_v3/pages/recipes/test.tsx
@@ -1,27 +1,15 @@
 import Footer from "../../components/Footer";
-
 import NavBar from "../../components/NavBar";
-import RecipeSidebar from "../../components/RecipeSidebar";
-
-import type {
-  NextPage,
-  GetServerSideProps,
-  InferGetServerSidePropsType,
-} from "next";
+import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import React, { useState } from "react";
-import prisma from "../../utils/prisma";
+
 import SignUpBanner from "../../components/SignUpBanner";
 import { Formik, Form, Field } from "formik";
-import { MiniNormalBoldArrow, NormalBoldArrow } from "../../utils/arrows";
 
-const NewIDPage: NextPage = ({
-  data,
-  avg_rating,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const NewIDPage: NextPage = () => {
   const { data: session } = useSession();
   const [comment, setComment] = useState(false);
-  const [clickedEdit, setClickedEdit] = useState(false);
 
   return (
     <>
@@ -116,21 +104,7 @@ const NewIDPage: NextPage = ({
               <p className="text-2xl mt-4 font-semibold">Comments</p>
 
               {-1 > 0 ? (
-                <div>
-                  {data?.comments.slice(0, 6).map((d: any, index: number) => (
-                    <div className="my-2 p-3 flex flex-row" key={index}>
-                      <div className="avatar">
-                        <div className="w-12 rounded-full mr-4 bg-stone-100"></div>
-                      </div>
-                      <div className="my-auto">
-                        <p>{d?.text}</p>
-                        <p className="text-sm text-stone-400">
-                          {d.createdAt.slice(5, 7)}/{d.createdAt.slice(2, 4)}
-                        </p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
+                <div></div>
               ) : (
                 <div className="mt-2 ">
                   <p>No comments</p>{" "}
@@ -142,52 +116,7 @@ const NewIDPage: NextPage = ({
                     View all comments
                   </p>
                 )}
-                {comment ? (
-                  <div className="mt-2">
-                    <Formik
-                      initialValues={{
-                        text: "",
-                      }}
-                      onSubmit={async (values) => {
-                        const res = await fetch("/api/create_comment", {
-                          method: "POST",
-                          headers: {
-                            Accept: "application/json",
-                          },
-                          body: JSON.stringify({
-                            recipeId: data.id,
-                            values: values,
-                            userEmail: session?.user?.email,
-                          }),
-                        });
-                      }}
-                    >
-                      <Form className="p-3 flex flex-col">
-                        <Field
-                          id="text"
-                          name="text"
-                          placeholder="Comment..."
-                          className="bg-stone-100 rounded p-2 my-2"
-                        />
-
-                        <div className="flex flex-row items-end ">
-                          <button
-                            className=" p-2 mr-3 my-2 rounded  font-semibold "
-                            onClick={() => setComment((comment) => !comment)}
-                          >
-                            Cancel
-                          </button>
-                          <button
-                            type="submit"
-                            className="bg-rosa p-2 my-2 rounded text-white font-semibold "
-                          >
-                            Submit
-                          </button>
-                        </div>
-                      </Form>
-                    </Formik>
-                  </div>
-                ) : (
+                {comment ? null : (
                   <button
                     className="p-2 mt-2 bg-stone-100 hover:bg-rosa hover:text-white font-semibold rounded-xl"
                     onClick={() => setComment((comment) => !comment)}
@@ -211,37 +140,6 @@ const NewIDPage: NextPage = ({
       </div>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async ({
-  query,
-}: any) => {
-  const thisRecipe = await prisma?.recipe.findUnique({
-    where: {
-      id: parseInt(query.id),
-    },
-    include: {
-      comments: true,
-      ratings: true,
-      likedBy: true,
-    },
-  });
-
-  const aggregations = await prisma?.rating.aggregate({
-    _avg: {
-      overallRating: true,
-    },
-    where: {
-      recipeId: parseInt(query.id),
-    },
-  });
-
-  return {
-    props: {
-      data: JSON.parse(JSON.stringify(thisRecipe)),
-      avg_rating: aggregations?._avg.overallRating,
-    },
-  };
 };
 
 export default NewIDPage;
