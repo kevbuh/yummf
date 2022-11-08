9fa710734a021c23f8c712a74b8be897ca089697
Kevin Buhler <kebu1560@colorado.edu>
2022-11-07 23:11:16 -0700

    update text on front page

--- web_v3/pages/index.tsx
+++ web_v3/pages/index.tsx
@@ -86,12 +86,12 @@ const Home: NextPage = () => {
               Get personalized recommendations so you always know what to cook.
             </p>
 
-            <Link href="https://www.kevinbuhler.com/yummf">
+            {/* <Link href="https://www.kevinbuhler.com/yummf">
               <p className="mx-auto font-medium text-gray-500 text-center mb-8 max-w-lg px-6 sm:px-0 underline">
                 This site works on local machines only. Click here to see a
                 showcase.
               </p>
-            </Link>
+            </Link> */}
 
             <form className="w-full sm:mx-auto">
               <div className="relative ">
